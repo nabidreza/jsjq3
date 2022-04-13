045345f35ae4a471d265a44301d358d08556b590
Nabid Reza <96846327+nabidreza@users.noreply.github.com>
2022-04-13 23:12:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+//js
+function  textChange(){
+	document.getElementById('h2').innerHTML = "hey!! It was a joke man"
+}
+
+
+
+
+//jquery
+$(document).ready(function(){
+
+	//alert
+	$('.button').click(function(){
+		alert("Your device is blasting")
+	});
+	//hide
+	$('.hide').click(function(){
+		$('h4').hide();
+	});
+	//show
+	$('.show').click(function(){
+		$('h4').show();
+	});
+	//toggle
+	$('.toggle').click(function(){
+		$('h4').toggle();
+	});
+	//fadein
+	$('.fadein').click(function(){
+		$('h4').fadeIn("fast");
+	});
+	//fadeout
+	$('.fadeout').click(function(){
+		$('h4').fadeOut("slow");
+	});
+	//fadetoggle
+	$('.fadetoggle').click(function(){
+		$('h4').fadeToggle(100);
+	});
+	//fadeto
+	$('.fadeto').click(function(){
+		$('h4').fadeTo(1000,.5);
+	});
+
+	//slide
+	$('.ques').mouseenter(function(){
+		$('.ans').slideDown("fast");
+	});
+	$('.ques').mouseout(function(){
+		$('.ans').slideUp(1000);
+	});
+
+
+
+
+	//practice from api documantation
+	//add ( + clone() + apend to() )
+	$('li').add('p').css("background-color","skyblue");
+	$('.add').css("background-color","black").add('.p1').css("color","blue");
+	$('.p1').clone().add("<span>veryy good</span>").appendTo(document.body).css("background-color","skyblue");
+
+	//addback ( +nextall() )
+	$('li.item2').nextAll().addBack().css("border","1px solid blue");
+	//addclass ( +last() )
+	$('.p2').last().addClass("selected highlited");
+	//after
+	$('.p2').after("<b>greeaaaattt!!!</b>");
+	$('.p2').after(document.createTextNode("hey"));
+});
